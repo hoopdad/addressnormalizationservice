d0438adba9854866fa23832a238e9ef6f48659c8
Mike O <mike@olivieris.com>
2020-01-22 17:20:15 -0500

    changed message

--- addressnormalizerservice.js
+++ addressnormalizerservice.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send('Hello Mike!'))
+app.get('/', (req, res) => res.send('Hello Mikey!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
